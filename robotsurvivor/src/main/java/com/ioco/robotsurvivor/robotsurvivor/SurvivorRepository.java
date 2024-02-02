a7953dfa5adcec2d63591753c8a55e9fde3da142
Robinson-Sat <156796180+Robinson-Sat@users.noreply.github.com>
2024-02-02 13:25:36 +0200

    Update SurvivorRepository.java
    Added reports

--- robotsurvivor/src/main/java/com/ioco/robotsurvivor/robotsurvivor/SurvivorRepository.java
+++ robotsurvivor/src/main/java/com/ioco/robotsurvivor/robotsurvivor/SurvivorRepository.java
@@ -2,5 +2,9 @@ package com.ioco.robotsurvivor.robotsurvivor;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SurvivorRepository extends JpaRepository<Survivor, Long> {
+    List<Survivor> findByIsInfected(boolean isInfected);
+
 }
