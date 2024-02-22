2120cb7d4e764cb0355093162046d68ea6918f53
Robonsin-Sat <Robinson.Satekge@gmail.com>
2024-02-22 20:53:49 +0200

    Update robot list

--- ROBOT-apocalypse/src/main/java/com/robot/apocalypse/rest_api/controller/RobotList_Controller.java
+++ ROBOT-apocalypse/src/main/java/com/robot/apocalypse/rest_api/controller/RobotList_Controller.java
@@ -1,7 +1,7 @@
 package com.robot.apocalypse.rest_api.controller;
 
 
-import com.robot.apocalypse.rest_api.service.RobotList_Service;
+import com.robot.apocalypse.rest_api.entity.Robot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -10,36 +10,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class RobotList_Controller {
 
     @Autowired
-    private RobotList_Service robotService;
+    //private RobotList_Service robotService;
 
     @Value("${external.api.url}")
     private String apiUrl;
 
-    @GetMapping("/ListOfRobots")
+    @GetMapping("/robotData")
     public ResponseEntity<String> getRobotData() {
         WebClient webClient = WebClient.create();
-
         String result = webClient.get()
                 .uri(apiUrl)
                 .retrieve()
                 .bodyToMono(String.class)
-                .block(); //
-
+                .block(); // blocking call, in a real application consider using reactive programming
         System.out.println("----------------");
         System.out.println(result);
         System.out.println("-----------");
-
         // Return the JSON in the HTTP response
         return ResponseEntity.ok()
                 .header("Content-Type", "application/json")
                 .body(result);
     }
-
-
-
+    @GetMapping("/flyingRobots")
+    public ResponseEntity<List<Robot>> getFlyingRobots() {
+        WebClient webClient = WebClient.create();
+        List<Robot> robots = webClient.get()
+                .uri(apiUrl)
+                .retrieve()
+                .bodyToFlux(Robot.class)
+                .doOnNext(robot -> System.out.println("Received Robot: " + robot))
+                .filter(robot -> "Flying".equals(robot.getCategory().name()))
+                .collectList()
+                .block();// call, in a real application consider using reactive programming
+        return ResponseEntity.ok()
+                .header("Content-Type", "application/json")
+                .body(robots);
+    }
+    @GetMapping("/landRobots")
+    public ResponseEntity<List<Robot>> getLandRobots() {
+        WebClient webClient = WebClient.create();
+        List<Robot> landRobots = webClient.get()
+                .uri(apiUrl)
+                .retrieve()
+                .bodyToFlux(Robot.class)
+                .filter(robot -> Robot.Category.Land.equals(robot.getCategory()))
+                .collectList()
+                .block(); // blocking call, in a real application consider using reactive programming
+        return ResponseEntity.ok()
+                .header("Content-Type", "application/json")
+                .body(landRobots);
+    }
 }
---
+++ ROBOT-apocalypse/src/main/java/com/robot/apocalypse/rest_api/entity/Robot.java
@@ -0,0 +1,58 @@
+package com.robot.apocalypse.rest_api.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class Robot {
+
+    private String model;
+    private String serialNumber;
+    //private ZonedDateTime manufacturedDate;
+    private String manufacturedDate;
+    private Category category;
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    public enum Category {
+        Flying, Land
+    }
+    public Robot() {
+    }
+    public Robot(String model, String serialNumber, String manufacturedDate, Category category) {
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.manufacturedDate = manufacturedDate;
+        this.category = category;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    public String getManufacturedDate() {
+        return manufacturedDate;
+    }
+    public void setManufacturedDate(String manufacturedDate) {
+        this.manufacturedDate = manufacturedDate;
+    }
+    public Category getCategory() {
+        return category;
+    }
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+    @Override
+    public String toString() {
+        return "Robot{" +
+                "model='" + model + '\'' +
+                ", serialNumber='" + serialNumber + '\'' +
+                ", manufacturedDate=" + manufacturedDate +
+                ", category=" + category +
+                '}';
+    }
+}
